0b7e36c21b6fe837e720f2bbd3191c3e402cda79
Eli Zat <eli.zatlawy@gmail.com>
2020-12-18 00:46:45 +0200

    bug fixed to the generate_report function

--- main.py
+++ main.py
@@ -27,14 +27,14 @@ def generate_report():
     all_results_list = json.load(all_results_file)
     Human_total_trans_in_db = 252
     Plant_total_trans_in_db = 72
-    outputFile = open("Results/Final_Report_remote_20_to_2_label0.txt", 'a+')
+    outputFile = open("Results/Final_Report.txt", 'a+')
     for results in all_results_list:
         outputFile.write("++++++++++++++++  Results report for minSup = " + str(results[0]) + " +++++++++++++++\n")
         outputFile.write("Total Number of itemSets found = " + str(len(results[1])) + "\n")
         for freqItem in results[1]:
             appearances_in_Humnan = 0
             appearances_in_Plant = 0
-            transactions_file = open("bio_data/United_transactions.csv", 'r')
+            transactions_file = open("bio_data/United_unsorted_trans.csv", 'r')
             for line in transactions_file:
                 all_found = True
                 for cog in freqItem[0]:
@@ -46,8 +46,8 @@ def generate_report():
                         appearances_in_Humnan += 1
                     else:
                         appearances_in_Plant += 1
-            percentage_in_human_bac = appearances_in_Humnan / Human_total_trans_in_db
-            percentage_in_plant_bac = appearances_in_Plant / Plant_total_trans_in_db
+            percentage_in_human_bac = round((appearances_in_Humnan / Human_total_trans_in_db)*100, 2)
+            percentage_in_plant_bac = round((appearances_in_Plant / Plant_total_trans_in_db)*100, 2)
             outputFile.write("Results for FreqItem: " + str(freqItem[0]) + '\n')
             outputFile.write("IG of FreqItem:  " + str(freqItem[1]) + '\n')
             outputFile.write("COG_INFO semantic description per each COG in the itemSet if exist:" + '\n')
@@ -59,6 +59,7 @@ def generate_report():
                     else:
                         for line in info_table_results:
                             outputFile.write(line)
+                    fp.seek(0)  # going back to the beginning of INFO_TABLE file
             outputFile.write("Number of occurrences in Human Bacteria genomes: " + str(appearances_in_Humnan) + '\n')
             outputFile.write("percentage of genomes that carry it in Human Bacteria genomes: " + str(percentage_in_human_bac) + "%" + '\n')
             outputFile.write("Number of occurrences in Plant Bacteria genomes: " + str(appearances_in_Plant) + '\n')
@@ -75,46 +76,64 @@ def generate_running_time_graph():
     """
     running_times_json = open("running_times.json", "r")
     running_times_list = json.load(running_times_json)
+    running_times_list.reverse()
     minSups_json = open("minSup_list.json", "r")
     minSups_list = json.load(minSups_json)
+    minSups_list.reverse()
     plt.plot(minSups_list, running_times_list, '--ro')
     plt.title("Minimum Support Effect on Running Time ")
     plt.xlabel("Minimum Support")
     plt.ylabel("Running Time")
-    plt.savefig('Results/Running_time_graph_30_to_2_remote.png')
+    plt.savefig('Results/Running_time_graph_reversed.png')
     plt.show()
 
 
 if __name__ == "__main__":
     # data_preprocess.data_to_transaction_file()
-    all_results_with_minSup = []
-    running_times = []
-    minSup_list = []
-    for j in range(20, 1, -2):  # backward loop from 50 to 2
-        minSup_list.append(j)
-        print("Creating DB.. MinSup =  ", j)
-        database = TransactionDatabase.loadFromFile("bio_data/United_unsorted_trans.csv", "0", j)
-        start = time.perf_counter()  # save starting time
-        miner = DDPMine(debug=False)
-        print("Starat mining MinSup = ", j)
-        results = miner.mine(database, j)
-        elapsed = time.perf_counter() - start  # calc running time for iteration
-        running_times.append(elapsed)
-        all_results_with_minSup.append([j, results])  # first item is the minSup, second is teh result
-        # with open("Results/Results_remote.txt", 'a') as f:
-        #     f.write("++++++++ Result for minSup = " + str(j) + "++++++++\n")
-        #     print("number of results found: ", len(results))
-        #     for item in results:
-        #         f.write("%s\n" % item)
-        #     f.write("number of results found: ")
-        #     f.write(str(len(results)) + "\n")
-    # Dump minSup, Running Times & All results into JSON files for post-process the results
-    with open('minSup_list.json', 'w') as f:
-        json.dump(minSup_list, f)
-    with open('running_times.json', 'w') as f:
-        json.dump(running_times, f)
-    with open('all_results_with_minSup.json', 'w') as f:
-        json.dump(all_results_with_minSup, f)
+    # all_results_with_minSup = []
+    # running_times = []
+    # minSup_list = []
+    # minSup = 324
+    # while minSup >= 5:
+    #     # 325 * 0.05 = 16
+    #     minSup = minSup - 16  # reduce the minSup by 5% of number of genomes in the original DB for each iteration
+    #     minSup_list.append(minSup)
+    #     print("Creating DB.. MinSup =  ", minSup)
+    #     database = TransactionDatabase.loadFromFile("bio_data/United_unsorted_trans.csv", "0", minSup)
+    #     start = time.perf_counter()  # save starting time
+    #     miner = DDPMine(debug=False)
+    #     print("Starat mining MinSup = ", minSup)
+    #     results = miner.mine(database, minSup)
+    #     elapsed = time.perf_counter() - start  # calc running time for iteration
+    #     running_times.append(elapsed)
+    #     all_results_with_minSup.append([minSup, results])  # first item is the minSup, second is teh result
+    # minSup = 4
+    # while minSup > 2:
+    #     minSup -= 1   # reduce the minSup by 2 for each iteration
+    #     minSup_list.append(minSup)
+    #     print("Creating DB.. MinSup =  ", minSup)
+    #     database = TransactionDatabase.loadFromFile("bio_data/United_unsorted_trans.csv", "0", minSup)
+    #     start = time.perf_counter()  # save starting time
+    #     miner = DDPMine(debug=False)
+    #     print("Starat mining MinSup = ", minSup)
+    #     results = miner.mine(database, minSup)
+    #     elapsed = time.perf_counter() - start  # calc running time for iteration
+    #     running_times.append(elapsed)
+    #     all_results_with_minSup.append([minSup, results])  # first item is the minSup, second is teh result
+    #     # with open("Results/Results_remote.txt", 'a') as f:
+    #     #     f.write("++++++++ Result for minSup = " + str(j) + "++++++++\n")
+    #     #     print("number of results found: ", len(results))
+    #     #     for item in results:
+    #     #         f.write("%s\n" % item)
+    #     #     f.write("number of results found: ")
+    #     #     f.write(str(len(results)) + "\n")
+    # # Dump minSup, Running Times & All results into JSON files for post-process the results
+    # with open('minSup_list.json', 'w') as f:
+    #     json.dump(minSup_list, f)
+    # with open('running_times.json', 'w') as f:
+    #     json.dump(running_times, f)
+    # with open('all_results_with_minSup.json', 'w') as f:
+    #     json.dump(all_results_with_minSup, f)
 
-    generate_report()  # generate detailed report from the DDPminer results
+    #generate_report()  # generate detailed report from the DDPminer results
     generate_running_time_graph()
